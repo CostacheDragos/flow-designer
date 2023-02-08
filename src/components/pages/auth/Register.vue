5fcccf988bab13bf199bb0866a556e25e17197d7
Dragos Costache <dcostache01@gmail.com>
2023-02-08 16:48:08 +0200

    Added validity checks in login and register inputs

--- src/components/pages/auth/Register.vue
+++ src/components/pages/auth/Register.vue
@@ -7,15 +7,24 @@
       <form class="space-y-6" action="#" @submit.prevent="onSubmit">
         <div>
           <label for="email" class="block mb-2 text-left text-sm font-medium text-white normal-case">Your email</label>
-          <input type="email" name="email" id="email" class="border rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="name@company.com" required="">
+          <input type="email" name="email" id="email" v-model="credentials.email.value"
+                 :class="credentials.email.displayError ? 'border-rose-600 focus:outline-rose-600 focus:border-rose-600' : 'border-gray-600 focus:outline-blue-500'"
+                 class="border rounded-lg block w-full p-2.5 bg-gray-700 placeholder-gray-400 text-white focus:outline focus:outline-2"
+                 placeholder="name@company.com" required @input="onEmailInput($event)" @invalid="onEmailInput($event)">
         </div>
         <div>
           <label for="password" class="block mb-2 text-left text-sm font-medium text-white normal-case">Password</label>
-          <input type="password" name="password" id="password" placeholder="••••••••" class="border rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" required="">
+          <input type="password" name="password" id="password" placeholder="••••••••" v-model="credentials.password.value"
+                 :class="credentials.password.displayError ? 'border-rose-600 focus:outline-rose-600 focus:border-rose-600' : 'border-gray-600 focus:outline-blue-500'"
+                 class="border rounded-lg block w-full p-2.5 bg-gray-700 placeholder-gray-400 text-white focus:outline focus:outline-2"
+                 required minlength="6" @input="onPasswordInput($event)" @invalid="onPasswordInput($event)">
         </div>
         <div>
           <label for="confirm-password" class="block mb-2 text-left text-sm font-medium text-white normal-case">Confirm Password</label>
-          <input type="password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="border rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" required="">
+          <input type="password" name="confirm-password" id="confirm-password" placeholder="••••••••" v-model="credentials.confirmPassword.value"
+                 :class="credentials.confirmPassword.displayError ? 'border-rose-600 focus:outline-rose-600 focus:border-rose-600' : 'border-gray-600 focus:outline-blue-500'"
+                 class="border rounded-lg block w-full p-2.5 bg-gray-700 placeholder-gray-400 text-white focus:outline focus:outline-2"
+                 required @input="onConfirmPasswordInput($event)" @invalid="onConfirmPasswordInput($event)">
         </div>
         <button type="submit" class="w-full bg-blue-600 hover:bg-blue-700 rounded text-white py-2">Sign up</button>
 
@@ -35,9 +44,44 @@
 
 <script setup>
 import {useUserStore} from "@/stores/user.js";
+import {reactive} from "vue";
 
 const { registerWithEmailAndPassword, loginWithGoogle } = useUserStore();
+
+const credentials = reactive({
+  email: {
+    value: "",
+    displayError: false,
+  },
+  password: {
+    value: "",
+    displayError: false,
+  },
+  confirmPassword: {
+    value: "",
+    displayError: false,
+  },
+});
+
 function onSubmit() {
-  registerWithEmailAndPassword("dcostache01@yahoo.com", "password");
+  registerWithEmailAndPassword(credentials.email, credentials.password);
+}
+
+// When the input changes, we check the contents in order
+// to provide visual feedback
+function onEmailInput(event) {
+  credentials.email.displayError = !event.target.validity.valid;
+}
+function onPasswordInput(event) {
+  credentials.password.displayError = !event.target.validity.valid;
+}
+function onConfirmPasswordInput(event) {
+  credentials.confirmPassword.displayError = credentials.password.value !== credentials.confirmPassword.value;
+
+  //The password confirmation input is invalid only if empty or not matching the password
+  if(credentials.confirmPassword.value === "")
+    event.target.setCustomValidity("");
+  else
+    event.target.setCustomValidity("Must match the password.");
 }
 </script>
